67bd39b8808c7c5360b275cc43c1c46bc7fdf19e
durga2706 <durkadevi0610@gmail.com>
2026-07-28 22:52:00 +0530

    Create c_problem4.c

---
+++ c_problem4.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student
+{
+    struct student *prev;
+    int id;
+    int Maths;
+    int Science;
+    struct student *next;
+};
+int main()
+{
+    struct student *head = NULL, *newnode, *temp, *ptr;
+    int i, choice, search, delid;
+    for(i = 1; i <= 5; i++)
+    {
+        newnode = (struct student *)malloc(sizeof(struct student));
+        printf("Enter ID : ");
+        scanf("%d", &newnode->id);
+        printf("Enter Maths : ");
+        scanf("%d", &newnode->Maths);
+        printf("Enter Science : ");
+        scanf("%d", &newnode->Science);
+        newnode->next = NULL;
+        newnode->prev = NULL;
+        if(head == NULL)
+        {
+            head = newnode;
+        }
+        else
+        {
+            temp = head;
+            while(temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = newnode;
+            newnode->prev = temp;
+        }
+    }
+    while(1)
+    {
+        printf("\n1.Insert Entry");
+        printf("\n2.Delete Entry");
+        printf("\n3.Display List");
+        printf("\n4.Exit");
+        printf("\nEnter Choice : ");
+        scanf("%d",&choice);
+        if(choice == 1)
+        {
+            printf("Enter ID after which insert : ");
+            scanf("%d",&search);
+            ptr = head;
+            while(ptr != NULL && ptr->id != search)
+            {
+                ptr = ptr->next;
+            }
+            if(ptr == NULL)
+            {
+                printf("ID Not Found\n");
+            }
+            else
+            {
+                newnode = (struct student *)malloc(sizeof(struct student));
+                printf("Enter New ID : ");
+                scanf("%d",&newnode->id);
+                printf("Enter Maths : ");
+                scanf("%d",&newnode->Maths);
+                printf("Enter Science : ");
+                scanf("%d",&newnode->Science);
+                newnode->next = ptr->next;
+                newnode->prev = ptr;
+                if(ptr->next != NULL)
+                {
+                    ptr->next->prev = newnode;
+                }
+                ptr->next = newnode;
+                printf("Inserted Successfully\n");
+            }
+        }
+        else if(choice == 2)
+        {
+            printf("Enter ID to Delete : ");
+            scanf("%d",&delid);
+            ptr = head;
+            while(ptr != NULL && ptr->id != delid)
+            {
+                ptr = ptr->next;
+            }
+
+            if(ptr == NULL)
+            {
+                printf("ID Not Found\n");
+            }
+            else
+            {
+                if(ptr->prev != NULL)
+                {
+                    ptr->prev->next = ptr->next;
+                }
+                else
+                {
+                    head = ptr->next;
+                }
+                if(ptr->next != NULL)
+                {
+                    ptr->next->prev = ptr->prev;
+                }
+                free(ptr);
+                printf("Deleted Successfully\n");
+            }
+        }
+        else if(choice == 3)
+        {
+            ptr = head;
+            while(ptr != NULL)
+            {
+                printf("\nID : %d", ptr->id);
+                printf("\nMaths : %d", ptr->Maths);
+                printf("\nScience : %d\n", ptr->Science);
+                ptr = ptr->next;
+            }
+        }
+        else if(choice == 4)
+        {
+            break;
+        }
+        else
+        {
+            printf("Invalid Choice\n");
+        }
+    }
+    return 0;
+}
